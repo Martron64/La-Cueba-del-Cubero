168cd29e5a906a0086bed799d1b5d670bf2f1443
Martron64 <jmarcosgan@gmail.com>
2026-01-02 16:20:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   actualizar();
 });
 
-function calcular(){
-let tabla = document.getElementById("tablaCubos");
-let fila = tabla.insertRow(); 
-fila.insertCell().textContent ="3X3";
-fila.insertCell().textContent ="26";
-fila.insertCell().textContent ="3";
-fila.insertCell().textContent ="6";
-fila.insertCell().textContent ="1";
-fila.insertCell().textContent ="7";
-fila.insertCell().textContent ="6";
-fila.insertCell().textContent ="12";
-fila.insertCell().textContent ="21";
-fila.insertCell().textContent ="0";
-fila.insertCell().textContent ="0";
-fila.insertCell().textContent ="0";
-fila.insertCell().textContent ="NO SE";
-}
-
 function actualizar(){
-  let CUBOS=[];
   fetch("DataCubos.json")
   .then(response => response.json())
   .then(data => {
@@ -51,6 +32,7 @@ for(let x=0;x<data.length;x++){
   fila.insertCell().textContent =data[x].difAlgTot+data[x].difBlock+data[x].difDeform+data[x].difParid;
 }
 }
+
 
 
 
